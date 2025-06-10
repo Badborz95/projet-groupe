500d37f964f852c69c3e7f209a4452a0d6d88ede
Badborz95 <93028562+Badborz95@users.noreply.github.com>
2025-06-10 11:05:16 +0200

    Navbar avec router

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-5 pt-5">
+    <h1>Accueil</h1>
+    <p>Bienvenue sur la page d'accueil</p>
+  </div>
+</template>
---
+++ src/views/Produit.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-5 pt-5">
+    <h1>Produit</h1>
+    <p>Voici la page produit</p>
+  </div>
+</template>
